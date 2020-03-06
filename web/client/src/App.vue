532b201684046207b34354d4124ff1a28be94faa
Stephen Ball <stephen.ball@lendinvest.com>
2020-03-06 13:42:26 +0000

    Help dialog

--- web/client/src/App.vue
+++ web/client/src/App.vue
@@ -35,17 +35,22 @@
       </b-collapse>
 
       <b-navbar-nav class="ml-auto">
-        <b-button variant="success" v-b-modal.aboutModal>About</b-button>
+        <b-button variant="success" v-b-modal.aboutModal>Help</b-button>
       </b-navbar-nav>
     </b-navbar>
 
     <viewer :namespace="namespace" :filter="filter" :autoRefresh="autoRefresh" ref="viewer"></viewer>
 
-    <b-modal id="aboutModal" title="About KubeView" header-bg-variant="info" header-text-variant="dark" ok-only>
+    <b-modal id="aboutModal" title="Help with KubeView" header-bg-variant="info" header-text-variant="dark" ok-only>
       <div class="text-center">
-      <img src="./assets/logo.png" width="100">
-      <p>v{{ version }} - Ben Coleman</p>
-      <b-button href="https://github.com/benc-uk/kubeview" target="_blank" variant="success">GitHub Project</b-button>
+        <img src="./assets/logo.png" width="100">
+
+        <p>Kubeview displays the current state of your cluster. Each resource is represented by a different icon, click them to see information on the resource.</p>
+        <p>Solid lines indicate that one resource <i>references</i> another in the direction of the arrow, a dashed line indicates that one resources <i>creates</i> another in the direction of the arrow.</p>
+
+        <b-button href="https://github.com/REBELinBLLUE/kubeview" target="_blank" variant="success">GitHub Project</b-button>
+
+        <p><br>Based on <a target="_blank" href="https://github.com/benc-uk/kubeview">KubeView by Ben Coleman</a></p>
       </div>
     </b-modal>
   </div>
