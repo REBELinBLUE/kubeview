e5f6f6874155c40b52d807958be85f9d4e9e9aa5
Stephen Ball <stephen@rebelinblue.com>
2020-03-07 21:59:01 +0000

    Fix typo

--- web/client/src/components/Help.vue
+++ web/client/src/components/Help.vue
@@ -6,7 +6,7 @@
       <p>KubeView displays the current state of your cluster. Each resource is represented by a different icon, click them to see information on the resource.</p>
       <p>Solid lines indicate that one resource <i>references</i> another in the direction of the arrow, a dashed line indicates that one resources <i>creates</i> another in the direction of the arrow.</p>
 
-      <b-button href="https://github.com/REBELinBLLUE/kubeview" target="_blank" variant="success">GitHub Project</b-button>
+      <b-button href="https://github.com/REBELinBLUE/kubeview" target="_blank" variant="success">GitHub Project</b-button>
 
       <p id="credit">Based on <a target="_blank" href="https://github.com/benc-uk/kubeview">KubeView by Ben Coleman</a></p>
     </div>
